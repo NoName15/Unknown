805c38739ca996aeddb735064f1dd037cbfbaacc
NoName15 <43522880+NoName15@users.noreply.github.com>
2018-10-07 21:28:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fetchVideoInfo = require('youtube-info');
 
 const YouTube = require('simple-youtube-api');
 
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const youtube = new YouTube("AIzaSyDLDoUv5Oca_Hd1QbTFL0jN6M6wWqn7njA");
 
 const queue = new Map();
 
